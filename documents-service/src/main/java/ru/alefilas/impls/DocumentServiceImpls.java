859dbd4a80441c2707cace48ef3f6c0c711d0fcf
Alexander <safil96@mail.ru>
2021-04-22 07:30:17 +0300

    Update service

--- documents-service/src/main/java/ru/alefilas/impls/DocumentServiceImpls.java
+++ documents-service/src/main/java/ru/alefilas/impls/DocumentServiceImpls.java
@@ -1,4 +1,38 @@
-package impls;
+package ru.alefilas.impls;
 
-public class DocumentServiceImpls implements DocumentService{
+import ru.alefilas.DocumentService;
+import ru.alefilas.DocumentsDao;
+import ru.alefilas.model.document.AbstractEntity;
+import ru.alefilas.model.document.Directory;
+import ru.alefilas.model.document.DocumentVersion;
+
+import java.util.List;
+
+public class DocumentServiceImpls implements DocumentService {
+
+    private DocumentsDao dao;
+
+    public DocumentServiceImpls(DocumentsDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public List<String> getAllDocumentTypes() {
+        return dao.findAllDocumentTypes();
+    }
+
+    @Override
+    public Long save(AbstractEntity entity) {
+        return dao.save(entity);
+    }
+
+    @Override
+    public Long save(DocumentVersion version) {
+        return dao.save(version);
+    }
+
+    @Override
+    public List<AbstractEntity> getEntitiesByDirectory(Directory directory) {
+        return dao.findEntityByDirectory(directory);
+    }
 }
